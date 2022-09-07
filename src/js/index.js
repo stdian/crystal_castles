e986e8a3a5a9730a23dc2dc70297b0dbe570e156
Dmitriy Stepanov <stdian@mail.ru>
2022-09-07 08:16:56 +0800

    add 6 block and footer

--- src/js/index.js
+++ src/js/index.js
@@ -2,45 +2,58 @@ import "../index.html"
 import "../en/index.html"
 import "../scss/index.scss"
 
-// ------------- VARIABLES ------------- //
-var ticking = false
-var isFirefox = /Firefox/i.test(navigator.userAgent)
-var isIe =
-	/MSIE/i.test(navigator.userAgent) ||
-	/Trident.*rv\:11\./i.test(navigator.userAgent)
-var scrollSensitivitySetting = 30 //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
-var slideDurationSetting = 600 //Amount of time for which slide is "locked"
-var currentSlideNumber = 0
-var totalSlideNumber = $(".background").length
+let ticking = false
+const isFirefox = /Firefox/i.test(navigator.userAgent)
+// eslint-disable-next-line no-useless-escape
+const isIe = /MSIE/i.test(navigator.userAgent) || /Trident.*rv\:11\./i.test(navigator.userAgent)
+const scrollSensitivitySetting = 30
+const slideDurationSetting = 600
+const mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel"
+let currentSlideNumber = 0
+// eslint-disable-next-line no-undef
+const totalSlideNumber = $(".background").length
 
-// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+const nextItem = () => {
+	// eslint-disable-next-line no-undef
+	const $previousSlide = $(".background").eq(currentSlideNumber - 1)
+	$previousSlide.removeClass("up-scroll").addClass("down-scroll")
+}
+
+const previousItem = () => {
+	// eslint-disable-next-line no-undef
+	const $currentSlide = $(".background").eq(currentSlideNumber)
+	$currentSlide.removeClass("down-scroll").addClass("up-scroll")
+}
+
+function slideDurationTimeout(slideDuration) {
+	setTimeout(() => {
+		ticking = false
+	}, slideDuration)
+}
+
 function parallaxScroll(evt) {
+	let delta
 	if (isFirefox) {
-		//Set delta for Firefox
-		var delta = evt.detail * -120
+		delta = evt.detail * -120
 	} else if (isIe) {
-		//Set delta for IE
-		var delta = -evt.deltaY
+		delta = -evt.deltaY
 	} else {
-		//Set delta for all other browsers
-		var delta = evt.wheelDelta
+		delta = evt.wheelDelta
 	}
 
-	if (ticking != true) {
+	if (ticking !== true) {
 		if (delta <= -scrollSensitivitySetting) {
-			//Down scroll
 			ticking = true
 			if (currentSlideNumber !== totalSlideNumber - 1) {
-				currentSlideNumber++
+				currentSlideNumber += 1
 				nextItem()
 			}
 			slideDurationTimeout(slideDurationSetting)
 		}
 		if (delta >= scrollSensitivitySetting) {
-			//Up scroll
 			ticking = true
 			if (currentSlideNumber !== 0) {
-				currentSlideNumber--
+				currentSlideNumber -= 1
 			}
 			previousItem()
 			slideDurationTimeout(slideDurationSetting)
@@ -48,24 +61,27 @@ function parallaxScroll(evt) {
 	}
 }
 
-// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
-function slideDurationTimeout(slideDuration) {
-	setTimeout(function () {
-		ticking = false
-	}, slideDuration)
-}
-
-// ------------- ADD EVENT LISTENER ------------- //
-var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel"
+// eslint-disable-next-line no-undef
 window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false)
 
-// ------------- SLIDE MOTION ------------- //
-function nextItem() {
-	var $previousSlide = $(".background").eq(currentSlideNumber - 1)
-	$previousSlide.removeClass("up-scroll").addClass("down-scroll")
-}
-
-function previousItem() {
-	var $currentSlide = $(".background").eq(currentSlideNumber)
-	$currentSlide.removeClass("down-scroll").addClass("up-scroll")
+window.scrollToSlide = (slide) => {
+	if (currentSlideNumber < slide) {
+		// eslint-disable-next-line no-undef
+		const slides = $(".background").slice(currentSlideNumber, slide)
+		for (let i = 0; i < slides.length; i += 1) {
+			slides[i].classList.remove("up-scroll")
+			slides[i].classList.add("down-scroll")
+		}
+		currentSlideNumber = slide
+		slideDurationTimeout(slideDurationSetting)
+	} else if (currentSlideNumber > slide) {
+		// eslint-disable-next-line no-undef
+		const slides = $(".background").slice(slide, currentSlideNumber)
+		for (let i = 0; i < slides.length; i += 1) {
+			slides[i].classList.add("up-scroll")
+			slides[i].classList.remove("down-scroll")
+		}
+		currentSlideNumber = slide
+		slideDurationTimeout(slideDurationSetting)
+	}
 }
